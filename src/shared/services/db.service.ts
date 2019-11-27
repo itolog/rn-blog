d9f5d74e4df886b1198c594d4066e4e5fd4d27b1
itolog <paukanlove@gmail.com>
2019-11-27 17:15:58 +0300

    add DB

--- src/shared/services/db.service.ts
+++ src/shared/services/db.service.ts
@@ -1,11 +1,8 @@
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { DATA } from '../data';
 
 class PostService {
   static loadPosts() {
-    return of(DATA);
+    return of([]);
   }
 }
 
